4fae7d810aef490ec4a23541ddebc12e6e3478f9
Danilo Cassiano Dias <dcassianodias@gmail.com>
2023-10-17 00:56:49 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/danilodev/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilodev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danilodev/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilodev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
