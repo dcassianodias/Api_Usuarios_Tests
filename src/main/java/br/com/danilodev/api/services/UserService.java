f6375f72154c2ab0963be76c3d0581a2bf7b1a30
Danilo Cassiano Dias <dcassianodias@gmail.com>
2023-10-29 22:50:02 -0300

    endpoint findAll para User

--- src/main/java/br/com/danilodev/api/services/UserService.java
+++ src/main/java/br/com/danilodev/api/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.danilodev.api.services;
 
 import br.com.danilodev.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
