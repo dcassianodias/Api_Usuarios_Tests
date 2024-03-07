830911affe6ecce90c974f4b1688fcdd55180b0b
Danilo Cassiano Dias <dcassianodias@gmail.com>
2024-03-06 22:02:13 -0300

    Test: teste para classe UserService, metodo create com sucesso.

--- src/test/java/br/com/danilodev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/danilodev/api/services/impl/UserServiceImplTest.java
@@ -104,6 +104,20 @@ class UserServiceImplTest {
     }
 
     @Test
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
     void update() {
     }
 
